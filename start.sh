75c95d0cf0b668345e168c2f3e9806342a1ca2ad
ksokala <sokala9@gmail.com>
2020-09-07 20:41:02 +0000

    Configuring CI/CD tools

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 useradd -u 1030 artifactory
 chown -R artifactory:artifactory volumes/artifactory
-useradd -u 1000 jenkins
-chown -R jenkins:jenkins volumes/jenkins
-# chmod -R 777 ./volumes/jenkins
+chown -R 1000:1000 volumes/jenkins
+touch volumes/jenkins/copy_reference_file.log
+chown 1000:1000 volumes/jenkins/copy_reference_file.log
 cd compose
 docker-compose up -d --build
